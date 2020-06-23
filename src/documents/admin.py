cc122becca9939d9820fac6fdba1703564c81d5b
dinhhung1598753 <hoangdinhhung159@gmail.com>
2020-06-23 10:39:43 -0400

    update document admin and views index

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import DocumentCategory, SubCategory, Tag, Document
 # Register your models here.
 
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title','number_of_view','number_of_download' ,'price')
+
+
 admin.site.register(DocumentCategory)
 admin.site.register(SubCategory)
 admin.site.register(Tag)
-admin.site.register(Document)
+admin.site.register(Document, DocumentAdmin)
