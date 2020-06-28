d2c8569922d0490d65f757e059016826dbb54c25
phongcubu <65958448+phongcubu@users.noreply.github.com>
2020-06-28 07:56:08 +0700

    đổi tên

--- src/myapp/apps.py
+++ src/myapp/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class MyappConfig(AppConfig):
     name = 'myapp'
+    # đổi tên
+    verbose_name ='Ứng Dụng'
+    verbose_name_plural = 'Ứng Dụng '
--- src/myapp/models.py
+++ src/myapp/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class DemoModel(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    image = models.ImageField(upload_to="demo_images")
+    title = models.CharField(max_length=255,verbose_name = "tiêu đề")
+    body = models.TextField(verbose_name = "viết mô tả")
+    image = models.ImageField(upload_to="demo_images",verbose_name = "hình ảnh")
 
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name_plural = 'Thông tin tài liệu'
